a1f91546d033aefd5c85ca962056e28913ca26e3
rahul4571 <rahul.hiragond@bynry.io>
2024-11-30 04:00:30 +0530

    last try

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://rbac-backend-two.vercel.app',
+    API_URL: 'https://rbac-backend-j8cddzbqs-rahul4507s-projects.vercel.app',
 };
