b78f32647d1e721ad490e323a562d0ae2e242020
rahul4571 <rahul.hiragond@bynry.io>
2024-11-30 14:53:44 +0530

    modified the backend api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://rbac-backend-j8cddzbqs-rahul4507s-projects.vercel.app',
+    API_URL: 'https://rbac-backend-vok1.onrender.com',
 };
