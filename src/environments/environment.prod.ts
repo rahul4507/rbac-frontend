822438c47efadd5d7383b3b375f902377bf4b011
rahul4507 <rahulhiragond04@gmail.com>
2024-12-02 23:03:59 +0530

    modified the env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    API_URL:"http://localhost:8080",
-    // API_URL: 'https://rbac-backend-vok1.onrender.com',
+    API_URL: 'https://rbac-backend-vok1.onrender.com',
 };
